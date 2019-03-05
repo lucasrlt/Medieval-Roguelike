6ed864a7faf28f01875d642b68d8f4fe418f760f
GUY HADRIEN p1710505 <hadrien.guy@etu.univ-lyon1.fr>
2019-03-05 10:30:35 +0100

    Création des tiles représentant les différents types de salles possible

---
+++ data/tilemaps/ExempleSalle.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="ExempleSalle" tilewidth="32" tileheight="32" tilecount="255" columns="17">
+ <image source="blanc.jpg" width="546" height="504"/>
+</tileset>
