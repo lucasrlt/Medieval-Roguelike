da822d92314517665adbc23195a52297931b03b5
ROLLET LUCAS p1709538 <lucas.rollet@etu.univ-lyon1.fr>
2019-03-05 11:21:08 +0100

    Début d'implémentation de la génération de niveau.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include "LevelGenerator.h"
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+
+    LevelGenerator lg;
+    lg.fetchRooms("../data/tilemaps");
+
     return 0;
 }
