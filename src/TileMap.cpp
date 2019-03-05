8780389ee2cc3b92f05609af1e97b604353ac52b
GUY HADRIEN p1710505 <hadrien.guy@etu.univ-lyon1.fr>
2019-03-05 11:49:17 +0100

    Implémentation en C++ des tiles

---
+++ src/TileMap.cpp
@@ -0,0 +1,5 @@
+//
+// Created by hadrien on 05/03/19.
+//
+
+#include "TileMap.h"
---
+++ src/TileMap.h
@@ -0,0 +1,14 @@
+//
+// Created by hadrien on 05/03/19.
+//
+
+#ifndef MEDIEVAL_ROGUELIKE_TILEMAP_H
+#define MEDIEVAL_ROGUELIKE_TILEMAP_H
+
+
+class TileMap {
+
+};
+
+
+#endif //MEDIEVAL_ROGUELIKE_TILEMAP_H
