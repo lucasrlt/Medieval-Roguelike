4aa51837610cfd78598823ad3e0e51d1d8d39d68
ROLLET LUCAS p1709538 <lucas.rollet@etu.univ-lyon1.fr>
2019-03-14 13:02:46 +0100

    Ajoute une valeur de retour à fetchRoomFromFile().

--- src/TileMap.cpp
+++ src/TileMap.cpp
@@ -10,38 +10,38 @@
 
 using namespace std;
 
-void TileMap::fetchRoomFromFile(const string& filename,int gridSize, int linesBeforeGrid)
+void TileMap::fetchRoomFromFile(const string &filename, int tabTile[GRID_SIZE][GRID_SIZE])
 {
-    ifstream readFile(filename.c_str());    //Ouverture d'un flux pour lire le fichier voulu
+    ifstream readFile(filename.c_str()); //Ouverture d'un flux pour lire le fichier voulu
 
-    if (!readFile.is_open()) {              //En cas d'erreur de l'ouverture du fichier, envoie un message d'erreur et stop le programme
+    if (!readFile.is_open())
+    { //En cas d'erreur de l'ouverture du fichier, envoie un message d'erreur et stop le programme
         cerr << "Erreur dans l'ouverture en lecture du fichier." << endl;
         return;
     }
 
-    if (readFile) {
+    if (readFile)
+    {
         char content;
         string beforeContent;
         char separator;
-        int tabTile[gridSize][gridSize];
 
-        for(int k = 0; k < linesBeforeGrid; k++)    //Récupère les premières lignes inutiles
+        for (int k = 0; k < LINES_BEFORE_GRID; k++) //Récupère les premières lignes inutiles
         {
             getline(readFile, beforeContent);
         }
 
-        for(int i = 0; i < gridSize; i++)   //Récupère les ids des tiles et les stocke dans un tableau 2D
+        for (int i = 0; i < GRID_SIZE; i++) //Récupère les ids des tiles et les stocke dans un tableau 2D
         {
-            for(int j = 0; j < gridSize; j++)
+            for (int j = 0; j < GRID_SIZE; j++)
             {
                 readFile >> content;
                 readFile >> separator;
                 tabTile[i][j] = content - '0';
-                //cout << tabTile[i][j];
             }
             cout << endl;
         }
-        readFile.close();   //Ferme le flux
+        readFile.close(); //Ferme le flux
     }
 }
 
@@ -57,21 +57,22 @@ void TileMap::getSpriteNames(vector<string> &files)
 
     ifstream readFile(filename.c_str());
 
-    if (!readFile.is_open()) {
+    if (!readFile.is_open())
+    {
         cerr << "Erreur dans l'ouverture en lecture du fichier." << endl;
         return;
     }
     while (!readFile.eof())
     {
         readFile >> testContent;
-        if (testContent =="<image") // Si le string correspond à <image, on récupère le nom du fichier et on le rentre dans le vector
+        if (testContent == "<image") // Si le string correspond à <image, on récupère le nom du fichier et on le rentre dans le vector
         {
             readFile >> separator;
             readFile >> separator;
             readFile >> sourceContent;
 
             size_t sourcePos = sourceContent.find("source="); //Cherche le mot source dans la chaine sourceContent
-            content = sourceContent.substr(sourcePos + 8); //Copie les caractères depuis le s de source jusqu'au " et le met dans le string content
+            content = sourceContent.substr(sourcePos + 8);    //Copie les caractères depuis le s de source jusqu'au " et le met dans le string content
             size_t contentPos = content.find('"');
             spriteContent = content.substr(0, contentPos);
             files.push_back(spriteContent);
--- src/TileMap.h
+++ src/TileMap.h
@@ -8,30 +8,34 @@
 #include <vector>
 
 using namespace std;
+
+const int TILES_WIDTH = 32;
+const int TILES_HEIGHT = 32;
+
+const int GRID_SIZE = 8;         // largeur et hauteur d'une salle en nombre de tiles
+const int LINES_BEFORE_GRID = 6; //
+
 /**
      @brief Classe qui récupère la couleur d'un sprite et la forme d'un tile
      */
+class TileMap
+{
+  public:
+    //Sprite[*] tiles;
 
-class TileMap {
-    public :
-        const int TILES_WIDTH = 32;
-        const int TILES_HEIGHT = 32;
-        //Sprite[*] tiles;
-
-        /**
+    /**
          * @brief Récupère la salle depuis le fichier
          * @param filename : nom du fichier du tile
          * @param gridSize  : Taille d'une salle de tiles
          * @param linesBeforeGrid : Lignes de textes du fichier filename avant les tiles
          */
-        void fetchRoomFromFile(const string& filename,int gridSize = 8, int linesBeforeGrid = 6); //Room* out_room);
-        //void drawTilesForRoom(Room& room, SDLMode& sdl);
-        /**
+    void fetchRoomFromFile(const string &filename, int tabTiles[GRID_SIZE][GRID_SIZE]); //Room* out_room;
+    //void drawTilesForRoom(Room& room, SDLMode& sdl);
+    /**
          * @brief Récupère les sprites des ids
          * @param files : tableau de vecteur string des sprites
          */
-        void getSpriteNames(vector<string> &files);
+    void getSpriteNames(vector<string> &files);
 };
-
 
 #endif //MEDIEVAL_ROGUELIKE_TILEMAP_H
