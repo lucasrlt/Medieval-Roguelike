7a199b1c54d062f59dfeabc9e7c09feb93301e53
PUILLET ALEXANDRE p1707082 <alexandre.puillet@etu.univ-lyon1.fr>
2019-03-14 16:27:06 +0100

    Ajout de la classe Game, et renomme la fonction generateDugeon

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include <string.h>
-#include "DungeonGenerator.h"
+#include "Game.h"
 
 using namespace std;
 
 int main() {
+    Game g;
+    g.initDungeon();
+
+    cout<<g.getConstRoom(5, 3).tilemapName<<endl;
 
     return 0;
 }
